a90e7bb41a8aa2ba69dbf63c1dd17cf0ddc20195
OuHaoyun <118020049@link.cuhk.edu.cn>
2023-09-20 13:43:17 +0800

    provide txt version 1

--- constants.py
+++ constants.py
@@ -1,1 +1,3 @@
-excel_path = ''
+excel_path = '/Users/haoyunou/Desktop/ms-security/研究院绩效考核/2023Q1&Q2研究院工作量统计/2023.1.1-1.31研究院工作量统计0213.xlsx'
+
+exp_path = '/Users/haoyunou/Desktop/ms-security/研究院绩效考核/exp-data/data-exp1.xlsx'
