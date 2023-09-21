55c23744ba15bd08e10dceb30f05c208bb429999
OuHaoyun <118020049@link.cuhk.edu.cn>
2023-09-21 16:23:55 +0800

    treat empty index

--- constants.py
+++ constants.py
@@ -1,3 +1,5 @@
-excel_path = '/Users/haoyunou/Desktop/ms-security/研究院绩效考核/2023Q1&Q2研究院工作量统计/2023.1.1-1.31研究院工作量统计0213.xlsx'
+okr_excel_path = '/Users/haoyunou/Desktop/ms-security/研究院绩效考核/2023Q1&Q2研究院工作量统计/2023.5.1-5.31研究院工作量统计0607.xlsx'
+researcher_info_excel_path = '/Users/haoyunou/Desktop/ms-security/研究院绩效考核/分析师列表（修正版）.xlsx'
 
 exp_path = '/Users/haoyunou/Desktop/ms-security/研究院绩效考核/exp-data/data-exp1.xlsx'
+
--- utils.py
+++ utils.py
@@ -1,5 +1,30 @@
 import pandas as pd
 
+def get_researcher_columns(df: pd.DataFrame) -> list:
+    """
+    Get the columns contains '研究员'
+    :param df: the DataFrame
+    :return: the columns contains '研究员'
+    """
+    researcher_columns = []
+    for col in df.columns:
+        if '研究员' in col:
+            researcher_columns.append(col)
+    return researcher_columns
+
+def treat_empty_xuhao(df: pd.DataFrame) -> pd.DataFrame:
+    """
+    Treat the empty values in '序号'
+    :param df: the DataFrame
+    :return: the DataFrame with no empty values in '序号'
+    """
+    special_list = []
+    for i in range(len(df)):
+        if pd.isna(df.loc[i, '序号']):
+            special_list.append(i)
+            df.loc[:, '序号'] = df['序号'].fillna(method='ffill')
+    return df
+
 def get_df_roadshow(df: pd.DataFrame) -> pd.DataFrame:
     """
     Get the roadshow DataFrame
@@ -7,21 +32,26 @@ def get_df_roadshow(df: pd.DataFrame) -> pd.DataFrame:
     :return: the roadshow DataFrame
     """
     # Define roadshow columns
-    researcher_columns = ['研究员', '研究员.1', '研究员.2', '研究员.3', '研究员.4', '研究员.5', '研究员.6']
-    service_columns = ['序号','所属团队', '服务事项', '客户机构', '客户分级', '客户区域']
+    service_columns = ['序号', '服务事项', '客户机构', '客户分级', '客户区域', '所属团队']
+    researcher_columns = get_researcher_columns(df)
     roadshow_columns = service_columns + researcher_columns
 
     # Read relavant columns and fillna
     df_temp = df[roadshow_columns].copy()
-    df_temp.loc[:, '序号'] = df_temp['序号'].fillna(method='ffill')
     df_temp.loc[:, '序号'] = df_temp['序号'].astype(int)
+    if df_temp['序号'].isna().sum() > 0:
+        df_temp = treat_empty_xuhao(df_temp)
+    df_temp.loc[:, '客户区域'] = df_temp['客户区域'].fillna(df_temp['客户机构'])
+    df_temp.loc[:, '客户分级'] = df_temp['客户分级'].fillna(df_temp['客户机构'])
 
     # Melt the df based on researcher columns
     df_temp_melted = pd.melt(df_temp, id_vars=service_columns, value_vars=researcher_columns, value_name='研究员姓名')
     df_temp_melted.drop('variable', axis=1, inplace=True) 
     df_temp_melted.dropna(subset=['研究员姓名'], inplace=True)
     df_temp_melted.rename(columns={'研究员姓名': '研究员'}, inplace=True)
-    df_temp_melted.loc[:, '所属团队'] = df_temp_melted['所属团队'].fillna(method='ffill')
+
+    # Map team based on researcher name
+    # todo
 
     # Sort the DataFrame based on '序号'
     df_temp_sorted = df_temp_melted.sort_values(by='序号')
